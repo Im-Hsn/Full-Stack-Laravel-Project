f046139da3494211e2f79bcdaad499650f08315d
Hassan <89822402+Im-Hsn@users.noreply.github.com>
2024-11-21 20:55:45 +0200

    Revert "Fixing the Merge error"
    This reverts commit 4e5b0793bb61f31c4ac87cff1220738ca680552a.

--- Property-Management-App/routes/web.php
+++ Property-Management-App/routes/web.php
@@ -28,10 +28,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/ListProperty',function(){
-    return view('propertylisting');
-    });
-
 // Test email route
 Route::get('/test-email/{status}/{bookingId}', function ($status, $bookingId) {
     Mail::to('host@example.com')->send(new BookingStatusMail($status, $bookingId));
